924932f1dc05d3179a7cc6e862c12537fa4f14ac
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-01 18:16:11 +0530

    Create Question 2.cpp

---
+++ Week 2/Question 2.cpp
@@ -0,0 +1,7 @@
+/*
+Given a sorted array of positive integers, design an algorithm and implement it using a program
+to find three indices i, j, k such that arr[i] + arr[j] = arr[k].
+*/
+
+#include<iostream>
+using namespace std;
