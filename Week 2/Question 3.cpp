6db1303ebe74d0409d08d18ed86f8c7ec9430923
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-01 18:17:18 +0530

    Create Question 3.cpp

---
+++ Week 2/Question 3.cpp
@@ -0,0 +1,7 @@
+/*
+Given an array of nonnegative integers, design an algorithm and a program to count the number
+of pairs of integers such that their difference is equal to a given key, K.
+*/
+
+#include<iostream>
+using namespace std;
