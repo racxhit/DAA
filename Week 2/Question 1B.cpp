6ac695aa9a909cd152fdf7071d44f44963248180
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-07 11:39:08 +0530

    Update Question 1B.cpp

--- Week 2/Question 1B.cpp
+++ Week 2/Question 1B.cpp
@@ -1,3 +1,11 @@
+/*
+Given a sorted array of positive integers containing few duplicate elements, design an algorithm
+and implement it using a program to find whether the given key element is present in the array
+or not. If present, then also find the number of copies of given key. (Time Complexity = O(log n))
+*/
+
+// Using Algorithm
+
 #include <iostream>
 #include <algorithm>
 using namespace std;
