a24a2eff386d2350ab91556e3c6ccd592324e3ba
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-08 08:55:40 +0530

    Update Question 1.cpp

--- Week 3/Question 1.cpp
+++ Week 3/Question 1.cpp
@@ -1,5 +1,8 @@
 /*
-
+Given an unsorted array of integers, design an algorithm and a program to sort the
+array using insertion sort. Your program should be able to find number of comparisons
+and shifts ( shifts - total number of times the array elements are shifted from their
+place) required for sorting the array.
 */
 
 #include<iostream>
