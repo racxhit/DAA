bc9b115891fa008cb5b9917ef19749f4eb07955d
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-08 08:57:02 +0530

    Update Question 3.cpp

--- Week 3/Question 3.cpp
+++ Week 3/Question 3.cpp
@@ -1,1 +1,10 @@
+/*
+Given an unsorted array of positive integers, design an algorithm and implement it
+using a program to find whether there are any duplicate elements in the array or not.
+(use sorting) (Time Complexity = O(n log n))
+*/
+
+#include<iostream>
+
+using namespace std;
 
