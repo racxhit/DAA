6cf4a6dcd2badcf9d7ad6035d52a3a3085ac82a2
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-08 08:55:57 +0530

    Update Question 2.cpp

--- Week 3/Question 2.cpp
+++ Week 3/Question 2.cpp
@@ -1,5 +1,7 @@
 /*
-
+Given an unsorted array of integers, design an algorithm and implement a program to
+sort this array using selection sort. Your program should also find number of
+comparisons and number of swaps required.
 */
 
 #include<iostream>
