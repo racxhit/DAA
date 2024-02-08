bc4b66b39cbe3a6ebbf6e16a970c61428d84a77c
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-08 21:37:51 +0530

    Update Question 1.cpp

--- Week 3/Question 1.cpp
+++ Week 3/Question 1.cpp
@@ -5,5 +5,58 @@ and shifts ( shifts - total number of times the array elements are shifted from 
 place) required for sorting the array.
 */
 
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+
+void insertionSort(int arr[], int n, int& comparisons, int& shifts)
+{
+    for (int i = 1; i < n; i++) 
+    {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) 
+        {
+            arr[j + 1] = arr[j];
+            j--;
+            shifts++;
+            comparisons++;
+        }
+        arr[j + 1] = key;
+        shifts++;
+    }
+}
+
+int main() 
+{
+    int T;
+    cin >> T;
+
+    while (T--) 
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) 
+        {
+            cin >> arr[i];
+        }
+
+        int comparisons = 0, shifts = 0;
+        insertionSort(arr, n, comparisons, shifts);
+
+        cout << "Sorted Array: ";
+        for (int i = 0; i < n; i++) 
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+
+        cout << "Comparisons: " << comparisons << endl;
+        cout << "Shifts: " << shifts << endl;
+    }
+
+    return 0;
+}
