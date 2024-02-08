8853411de71e76bd7f9bc8dde8f6bc7d365cebda
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-08 08:10:46 +0530

    Update Question 2.cpp

--- Week 2/Question 2.cpp
+++ Week 2/Question 2.cpp
@@ -3,5 +3,59 @@ Given a sorted array of positive integers, design an algorithm and implement it 
 to find three indices i, j, k such that arr[i] + arr[j] = arr[k].
 */
 
-#include<iostream>
+#include <iostream>
+
 using namespace std;
+
+bool findSequence(int arr[], int n) 
+{
+    for (int k = n - 1; k >= 0; k--) 
+    {
+        int i = 0;
+        int j = k - 1;
+
+        while (i < j) 
+        {
+            int sum = arr[i] + arr[j];
+
+            if (sum == arr[k]) 
+            {
+                cout << i << " " << j << " " << k << endl;
+                return true;
+            } 
+            else if (sum < arr[k]) 
+            {
+                i++;
+            } 
+            else 
+            {
+                j--;
+            }
+        }
+    }
+
+    cout << "No sequence found" << endl;
+    return false;
+}
+
+int main() 
+{
+    int T;
+    cin >> T;
+
+    while (T--) 
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) 
+        {
+            cin >> arr[i];
+        }
+
+        findSequence(arr, n);
+    }
+
+    return 0;
+}
