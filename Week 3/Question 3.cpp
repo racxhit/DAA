dc701a73da6907b4302747fbf61f5a4e2ad98b5f
Rachit Sharma <113026035+racxhit@users.noreply.github.com>
2024-02-29 08:42:54 +0530

    Update Question 3.cpp

--- Week 3/Question 3.cpp
+++ Week 3/Question 3.cpp
@@ -4,7 +4,49 @@ using a program to find whether there are any duplicate elements in the array or
 (use sorting) (Time Complexity = O(n log n))
 */
 
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+string hasDuplicates(vector<int>& arr) 
+{
+    sort(arr.begin(), arr.end());
+
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) 
+    {
+        if (arr[i] == arr[i + 1]) 
+        {
+            return "YES"; 
+        }
+    }
+
+    return "NO"; 
+}
+
+int main() 
+{
+    int T;
+    cin >> T;
+
+    while (T--) 
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) 
+        {
+            cin >> arr[i];
+        }
+
+        string result = hasDuplicates(arr);
+
+        cout << result << endl;
+    }
+
+    return 0;
+}
 
